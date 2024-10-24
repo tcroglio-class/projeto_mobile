b4eed19defa8deddbfcf69817ecd978112cc5902
tcroglio-class <tiago.roglio2112@gmail.com>
2024-10-24 20:17:08 -0300

    update

--- src/layouts/TelaPrincipalAcad.tsx
+++ src/layouts/TelaPrincipalAcad.tsx
@@ -6,28 +6,23 @@ import Sidebar from '../components/Sidebar';
 
 
 const TelaPrincipalAcad = (props: PrincipalAcadProps) => {
-
-
-	function constroiTela(elemento: number) {
-		if (elemento == 1) {
-			props.navigation.navigate(
-				'TelaElementos'
-			)
-		} else if (elemento == 2) {
-			props.navigation.navigate(
-				'TelaCadastroProduto',
-			)
-		} else if (elemento == 3) {
-			props.navigation.navigate(
-				'TelaConsProduto',
-			)
-		}
+	function constroiTela() {
+		props.navigation.navigate(
+			'TelaCadastroAlunoAcad',
+		)
 	}
 
 	return (
 		<Sidebar navigation={props.navigation}>
 			<View style={{ padding: 10, gap: 20, marginTop: 230 }}>
 				<Text style={[styles.texto_botao, { color: 'black' }]}>NOTICIAS</Text>
+				<Pressable
+					style={{ backgroundColor: 'green', padding: 5, borderRadius: 10 }}
+					onPress={() => { constroiTela() }}
+				>
+					<Text style={{ fontSize: 30, textAlign: 'center' }}>Cadastrar aluno</Text>
+				</Pressable>
+
 			</View>
 		</Sidebar>
 	);
